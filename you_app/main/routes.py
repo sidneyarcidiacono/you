de9e8e127136f9586e1c663ff27fff9b6fe0c0f0
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-21 14:16:22 -0800

    Skeleton routes for goals, challenges, progress & skeleton templates for these.

--- you_app/main/routes.py
+++ you_app/main/routes.py
@@ -8,3 +8,21 @@ main = Blueprint("main", __name__)
 def homepage():
     """Show user the homepage."""
     return render_template("index.html")
+
+
+@main.route("/challenges")
+def challenges():
+    """Show user available and current challenges."""
+    return render_template("challenges.html")
+
+
+@main.route("/progress")
+def progress():
+    """Show user current progress in challenges."""
+    return render_template("progress.html")
+
+
+@main.route("/goals")
+def goals():
+    """Show user visual representations of their goals."""
+    return render_template("goals.html")
