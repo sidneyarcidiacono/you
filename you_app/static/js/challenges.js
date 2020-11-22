8f658aff784a62ff29cf53b36e57706d991f0426
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-21 17:00:05 -0800

    Added add challenge modal, JS to handle actions.

---
+++ you_app/static/js/challenges.js
@@ -0,0 +1,32 @@
+const startModalBtn = document.getElementById('start-modal-btn')
+const addChallengeModal = document.getElementById('add-challenge-modal')
+const saveChallengeBtn = document.getElementById('save-add-challenge')
+const stopAddChallengeBtn = document.getElementById('stop-add-challenge')
+const closeButton = document.getElementById('x-button')
+const backdrop = document.getElementById('modal-backdrop')
+
+
+const backdropHandler = () => {
+  backdrop.classList.toggle('visible')
+}
+
+const challengeModalHandler = () => {
+  backdropHandler()
+  addChallengeModal.style.display = 'block'
+}
+
+const cancelAddHandler = () => {
+  addChallengeModal.style.display = 'none'
+  backdropHandler()
+}
+
+const saveChallengeHandler = () => {
+  addChallengeModal.style.display = 'none'
+  backdropHandler()
+}
+
+startModalBtn.addEventListener('click', challengeModalHandler)
+saveChallengeBtn.addEventListener('click', saveChallengeHandler)
+stopAddChallengeBtn.addEventListener('click', cancelAddHandler)
+closeButton.addEventListener('click', cancelAddHandler)
+backdrop.addEventListener('click', backdropHandler)
