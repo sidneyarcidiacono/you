f0ed14fbe2ce8ecd683afce62f2c37a3bdcb5852
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-24 10:58:40 -0800

    Updating add goal user flow on front end.

--- you_app/goals/routes.py
+++ you_app/goals/routes.py
@@ -1,6 +1,6 @@
 """Package & module import."""
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 
 goal = Blueprint("goal", __name__)
 
@@ -12,3 +12,10 @@ goal = Blueprint("goal", __name__)
 def goals():
     """Show user visual representations of their goals."""
     return render_template("goals.html")
+
+
+@goal.route("/add_goal", methods=["POST"])
+@login_required
+def add_goal():
+    """Add goal for current_user."""
+    pass
--- you_app/static/js/goal.js
+++ you_app/static/js/goal.js
@@ -1,3 +1,4 @@
+// Global constants for opening and closing modal event listeners
 const backdrop = document.getElementById('modal-backdrop')
 const addGoalBtn = document.getElementById('start-modal-btn')
 const addGoalModal = document.getElementById('add-goal-modal')
@@ -5,6 +6,11 @@ const xModalButton = document.getElementById('x-goal-button')
 const saveGoalBtn = document.getElementById('save-add-goal')
 const closeModalBtn = document.getElementById('stop-add-goal')
 
+// Global constants for showing and hiding form portions
+const category = document.getElementById('category-select')
+
+
+// Handlers for open and close modal event listeners
 const backdropHandler = () => {
   backdrop.classList.toggle('visible')
 }
@@ -24,6 +30,25 @@ const saveAddGoalHandler = () => {
   backdropHandler()
 }
 
+// Handlers for showing and hiding portions of add goal form
+const finishAddGoalHandler = () => {
+  const goalLabel = document.getElementById('goal-label')
+  const hiddenForm = document.getElementById('goal-form-part-two')
+  if (category.value == "water-intake") {
+    goalLabel.innerHTML = "How many ounces per day?"
+  } else if (category.value == "steps") {
+    goalLabel.innerHTML = "How many steps per day?"
+  } else if (category.value == "heart-rate") {
+    goalLabel.innerHTML = "What would you like your heart rate to be at rest? (Beats per minute)"
+  } else if (category.value == "calories") {
+    goalLabel.innerHTML = "How many calories per day?"
+  }
+  hiddenForm.classList.remove('invisible')
+}
+
+category.addEventListener('change', finishAddGoalHandler)
+
+// Event listeners for opening and closing modal
 addGoalBtn.addEventListener('click', addGoalModalHandler)
 saveGoalBtn.addEventListener('click', saveAddGoalHandler)
 xModalButton.addEventListener('click', cancelAddHandler)
