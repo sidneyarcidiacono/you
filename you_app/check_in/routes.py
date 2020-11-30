43a00ce8af11f89ae9a286db4f7e438fda2d7866
Sidney Arcidiacono <sidneyarci@gmail.com>
2020-11-30 09:48:56 -0800

    Updated get started/settings dropdown flow for clarity for new users

--- you_app/check_in/routes.py
+++ you_app/check_in/routes.py
@@ -1,5 +1,5 @@
 """Package & module import."""
-from flask import Blueprint, request, redirect, url_for
+from flask import Blueprint, request, redirect, url_for, jsonify
 from flask_login import current_user, login_required
 from you_app.models import Checkin, Goal
 from you_app import db
@@ -18,12 +18,30 @@ def add_check_in():
     for_goal = Goal.query.filter_by(
         user_id=current_user.id, category=category
     ).first()
-    print(for_goal.set_daily_percent_completed(amt_completed))
-    print(f"Expected daily improvement {for_goal.daily_expected_improvement}")
-    print(f"Baseline: {for_goal.user_baseline}")
+    for_goal.set_daily_percent_completed(amt_completed)
     check_in = Checkin(
         category=category, amt_completed=amt_completed, goal_id=for_goal.id
     )
     db.session.add(check_in)
     db.session.commit()
     return redirect(url_for("main.homepage"))
+
+
+@check_in.route("/check_in")
+@login_required
+def get_goals():
+    """Submit goal information for current user to front end."""
+    goal_data = {}
+    user_goals = Goal.query.filter_by(user_id=current_user.id)
+    for goal in user_goals:
+        goal_data[goal.id] = goal.category
+    return jsonify(goal_data), 200
+    if not user_goals:
+        return jsonify(
+            {
+                "msg": """
+                        Uh oh, you have to have goals set to check in!
+                        Try adding some!
+                        """
+            }
+        )
